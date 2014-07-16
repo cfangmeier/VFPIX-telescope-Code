71eebf30aecb599530e9697ceb8cb8b5ea42f02e
Caleb Fangmeier <cfangmeier74@gmail.com>
2014-07-16 17:49:08 -0500

    Complete rewrite of firmware code. Written to be much less general. This is a consequence of the limit of the logic elements on the CPLD.

--- firmware/Control_Unit.vhd
+++ firmware/Control_Unit.vhd
@@ -8,119 +8,141 @@ use ieee.numeric_std.all;
 -- Basic idea: Read data from onboard memory and write that data
 --             to the pattern buffers for each channel. 
 
-entity Control_Unit is
-	port (
+ENTITY Control_Unit IS
+	PORT (
 	      -- Input clock/reset
-	      clk: in std_logic;
-			res: in std_logic;
+	      clk: IN std_logic;
 			
-	      -- Input from memory block
-	      mem_data: in std_logic_vector(15 downto 0);
-	      nbusy, data_valid, osc: in std_logic;
+			-- Command input
+			pattern_select: IN std_logic_vector(3 DOWNTO 0);
 			
-			-- Memory Control Signals
-	      mem_addr: out std_logic_vector(11 downto 0);
-			nread: out std_logic;
-			
-			-- Channel Control Signals
-			channel_select: out std_logic_vector(7 downto 0);
-			pattern_select: out std_logic_vector(1 downto 0);
-			channel_data: out std_logic_vector(15 downto 0);
-			sel_patN: out std_logic;
-			en: out std_logic;
-			wr: out std_logic
+			-- Output channels
+			LE   : OUT std_logic;
+			RBI  : OUT std_logic;
+			RP1  : OUT std_logic;
+			RP2  : OUT std_logic;
+			SE   : OUT std_logic;
+			SBI  : OUT std_logic;
+			SP1  : OUT std_logic;
+			SP2  : OUT std_logic;
+			CAL  : OUT std_logic;
+			IS1  : OUT std_logic;
+			IS2  : OUT std_logic;
+			SR   : OUT std_logic;
+			CS   : OUT std_logic;
+			RES  : OUT std_logic;
+			R12  : OUT std_logic
 			);
 		  
-end Control_Unit;
+END Control_Unit;
 
 
-architecture Control_Unit_arch of Control_Unit is
+ARCHITECTURE Control_Unit_arch OF Control_Unit IS
 	
-	shared variable stage: integer := 0;
-	-- Stage 0: init outputs
-	-- Stage 1: read sel into channels
-	-- Stage 2: read pattern into channels
-	-- Stage 3: enable output/choose pattern
-	-- Stage 4: loop
+	SIGNAL first_clk: bit := '1';
+	SIGNAL out_select: std_logic_vector(3 DOWNTO 0) := "0000";
 	
-	shared variable patternN: integer := 0;
-	shared variable channelN: integer := 0;
-	
-	shared variable read_stage: integer := 0;
-	-- Read Stage 0: issue read of memory
-	-- Read Stage 1: read memory and put data on port
-	-- Read Stage 2: push data out port
-	-- Read Stage 3: end read
-	
-	shared variable load_stage: integer := 0;
-	
-begin
+BEGIN
 
-	process (clk, res)
-	begin
+	PROCESS (clk, res)
+	BEGIN
 	
 	-- only work on positive clock
-	if(rising_edge(clk)) then
+	IF(rising_edge(clk)) THEN
 
-		if(stage = 0) then
-			wr <= '0';
-			en <= '0';
-			nread <= '1';
-			stage := 1;
-		elsif(stage = 1) then
-			if(nbusy = '1') then
-				case read_stage is
-					when 0 => mem_addr <= X"000";
-								 nread <= '0';
-								 read_stage := 1;
-					when 1 => nread <= '1';
-					          sel_patN <= '1';
-					          channel_data(15 downto 0) <= mem_data(15 downto 0);
-								 read_stage := 2;
-					when 2 => wr <= '1';
-								 read_stage := 3;
-					when 3 => wr <= '0';
-								 read_stage := 0;
-								 stage := 2;
-					when others => null;
-				end case;
-			end if; -- busy
-		elsif(stage = 2) then
-		  if(nbusy = '1') then
-		  		case read_stage is
-				
-					when 0 => mem_addr <= std_logic_vector(to_unsigned(2+channelN*8+patternN*2, mem_addr'length));
-								 nread <= '0';
-								 read_stage := 1;
-					when 1 => nread <= '1';
-					          channel_data(15 downto 0) <= mem_data(15 downto 0);
-					          sel_patN <= '0';
-								 channel_select <= std_logic_vector(shift_left(to_unsigned(1,channel_select'LENGTH),channelN));
-								 pattern_select <= std_logic_vector(to_unsigned(patternN, pattern_select'length));
-								 read_stage := 2;
-					when 2 => wr <= '1';
-								 read_stage := 3;
-					when 3 => wr <= '0';
-					          if(patternN = 3 and channelN = 7) then
-									  stage := 3;
-								 elsif(patternN = 3) then
-									  patternN := 0;
-									  channelN := channelN + 1;
-								 else
-									  patternN := patternN + 1;
-								 end if; --patternN is 3
-								 read_stage := 0;
-					when others => null;
-				end case;
-		  end if; --busy
-		elsif(stage = 3) then
-			pattern_select <= "00";
-			en <= '1';
-			stage := 4;
-		else
---			-- do nothing.
-		end if; -- stage
+		IF(first_clk = '0') THEN
+			first_clk <= '1';
+			-- Set appropriate out_select here
+			out_select <= pattern_select;
+		ELSE
+			first_clk <= '0';
+		END IF;
 	
-	end if; -- clock cycle
-	end process;
-end Control_Unit_arch;
+		CASE out_select IS
+			WHEN "0000" => -- Reset
+				IF(first_clk = '1') THEN
+					LE   <= '0';
+					RBI  <= '0';
+					RP1  <= '1';
+					RP2  <= '0';
+					SE   <= '1';
+					SBI  <= '0';
+					SP1  <= '1';
+					SP2  <= '0';
+					CAL  <= '0';
+					IS1  <= '0';
+					IS2  <= '0';
+					SR   <= '0';
+					CS   <= '0';
+					RES  <= '1';
+					R12  <= '0';
+				ELSE
+					LE   <= '0';
+					RBI  <= '0';
+					RP1  <= '0';
+					RP2  <= '1';
+					SE   <= '1';
+					SBI  <= '0';
+					SP1  <= '0';
+					SP2  <= '1';
+					CAL  <= '0';
+					IS1  <= '0';
+					IS2  <= '0';
+					SR   <= '0';
+					CS   <= '0';
+					RES  <= '1';
+					R12  <= '0';
+				END IF; -- 0000
+			WHEN "0001" => -- Take Data - Stage 1
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "0010" => -- Take Data - Stage 2
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "0011" => -- Calibration - Stage 1
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "0100" => -- Calibration - Stage 2
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "0101" => -- Readout - Stage 1a
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "0110" => -- Readout - Stage 1b
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "0111" => -- Readout - Stage 2a
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN "1000" => -- Readout - Stage 2b
+				IF(first_clk = '1') THEN
+				
+				ELSE
+				
+				END IF; -- 0000
+			WHEN OTHERS => 
+		END CASE;
+	END IF; -- clock cycle
+	END PROCESS;
+END Control_Unit_arch;
