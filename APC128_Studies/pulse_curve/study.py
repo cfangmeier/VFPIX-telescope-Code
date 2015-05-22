4e9b752d98e68ea755acc187b7b4cb6acf2425c8
Caleb Fangmeier <cfangmeier74@gmail.com>
2015-05-22 14:30:31 -0500

    Adds re-read pattern

--- APC128_Studies/pulse_curve/study.py
+++ APC128_Studies/pulse_curve/study.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import os
 import re
 from collections import defaultdict
@@ -10,13 +9,9 @@ import matplotlib.pyplot as plt
 import seaborn
 seaborn.set()
 
-PERIOD = 0.769E-6
-OFFSET = -PERIOD * .25
-SIDE = PERIOD * .2
-
 
 def get_selection_times(t0, delay):
-    ts = [t0 + PERIOD*(i+delay/24)
+    ts = [t0 + PERIOD*(i+delay/CLKS_IN_PERIOD)
           for i in range(5)]
     ts = [(t1+OFFSET+SIDE, t2+OFFSET-SIDE) for (t1, t2) in
           zip(iter(ts[:-1]), iter(ts[1:]))]
@@ -24,55 +19,80 @@ def get_selection_times(t0, delay):
 
 
 def plot_selection(df, ts):
-    plt.plot(df.Time,df.Ampl, 'b.')
+    plt.plot(df.Time,df.Ampl, 'b')
     for t1, t2 in ts:
         df_sel = df.query("{} < Time < {}".format(t1, t2))
         plt.plot(df_sel.Time, df_sel.Ampl, 'g.')
     plt.show()
 
 
-def get_pulse_curve(R12):
+def read_data_short(R12):
+    global PERIOD, OFFSET, SIDE, CLKS_IN_PERIOD
     data_dir = "./data_{}/".format(R12)
     filenames = os.listdir(data_dir)
     filenames = [data_dir+filename for filename in filenames]
     filenames.sort()
+    PERIOD = 0.769E-6
+    OFFSET = -PERIOD * .25
+    SIDE = PERIOD * .2
+    CLKS_IN_PERIOD=24
 
-    levels = defaultdict(list)
-    level_errors = defaultdict(list)
-    delays = []
     for filename in filenames:
+        delay = int(re.search("0\d+", filename).group())
         if R12:
             df = pd.read_csv(filename)
         else:
             df = pd.read_csv(filename, skiprows=4)
-        delay = int(re.search("0\d+", filename).group())
-
         if delay > 70:
             delay = 70 + 10*(delay-70)
+        yield delay, df
 
+
+def read_data_full():
+    global PERIOD, OFFSET, SIDE, CLKS_IN_PERIOD
+    data_dir = "./data_2/"
+    filenames = os.listdir(data_dir)
+    filenames = [data_dir+filename for filename in filenames]
+    filenames.sort()
+    PERIOD = 7.68E-7
+    OFFSET = PERIOD * .75
+    SIDE = PERIOD * .2
+    CLKS_IN_PERIOD = 24
+
+    for filename in filenames:
+        delay = int(re.search("0\d+", filename).group())*4
+        df = pd.read_csv(filename, skiprows=4)
+        yield delay, df
+
+
+def get_pulse_curve(gen):
+
+    levels = defaultdict(list)
+    level_errors = defaultdict(list)
+    delays = []
+    for delay, df in gen:
         ts = get_selection_times(df.Time[0], delay)
         # plot_selection(df, ts)
         for i, (t1, t2) in enumerate(ts):
-            sel_str = "{}<Time<{}".format(t1,t2)
+            sel_str = "{}<Time<{}".format(t1, t2)
             df_sel = df.query(sel_str)
             levels[i].append(df_sel.Ampl.mean())
             level_errors[i].append(df_sel.Ampl.std())
-        delays.append(1E6*delay * PERIOD/24)
+        delays.append(1E6*delay * PERIOD/CLKS_IN_PERIOD)
 
     return delays, levels, level_errors
 
 
-def main():
-    fig, axs = plt.subplots(nrows=2,ncols=2,
-                            sharex=True,sharey=True)
+def step_curve_short():
 
-    for R12 in (0,1):
-        delays, levels, level_errors = get_pulse_curve(R12)
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
+    for R12 in (0, 1):
+        gen = read_data_short(R12)
+        delays, levels, level_errors = get_pulse_curve(gen)
         for i in levels.keys():
             level = np.array(levels[i])*1000
             level_error = np.array(level_errors[i])*1000
-            axs[i//2,i%2].plot(delays, level,
-                        label="R12={}".format(R12))
+            axs[i//2, i%2].plot(delays, level, label="R12={}".format(R12))
     axs[0,0].legend(loc='upper left')
     axs[0,0].set_title('Height 1')
     axs[0,1].set_title('Height 2')
@@ -81,11 +101,24 @@ def main():
 
     fig.suptitle("CAL Pulse Curve")
     fig.text(0.5, 0.02, "delay($\mu$s)", ha='center', va='center')
-    fig.text(0.01, 0.5, "Amplitude(mV)", ha='center', va='center',
-             rotation='vertical')
+    fig.text(0.01, 0.5, "Amplitude(mV)", ha='center', va='center', rotation='vertical')
     plt.tight_layout()
     plt.show()
 
+def step_curve_full():
+    gen = read_data_full()
+    delays, levels, level_errors = get_pulse_curve(gen)
+    for i in levels.keys():
+        level = np.array(levels[i])*1000
+        level_error = np.array(level_errors[i])*1000
+        plt.plot(delays, level, '.-')
+
+    plt.title("CAL Pulse Curve(Re-read pattern)")
+
+    plt.xlabel("delay($\mu$s)")
+    plt.ylabel("Amplitude(mV)")
+    plt.show()
 
 if __name__ == "__main__":
-    main()
+    # step_curve_full()
+    step_curve_short()
